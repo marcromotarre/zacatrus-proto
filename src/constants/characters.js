8e7482ddfadae5590ea2d20a33ce182d9786fd9c
Marc Romo <marc.romo@attackiq.com>
2022-07-17 22:58:08 +0200

    obscurio

--- src/constants/characters.js
+++ src/constants/characters.js
@@ -1,5 +1,5 @@
 import { invisibleManDice, timeManDice } from './dices.js'
-import { invisiblePunch, ironLever, kick, punch } from './faces.js'
+import { invisibility, invisiblePunch, ironLever, kick, punch } from './faces.js'
 
 const invisibleMan = {
   name: 'MAURICE JACOB',
@@ -56,9 +56,155 @@ const invisibleMan = {
         ],
       },
     ],
-    combo: [],
-    special: [],
-    fatalities: [],
+    combo: [
+      {
+        name: 'COMBO #1',
+        description: 'Combo #1 descripción',
+        dices: [
+          { face: punch },
+          { face: punch },
+          { face: punch },
+          { face: kick },
+          { face: kick, damage: '3' },
+        ],
+      },
+      {
+        name: 'COMBO #2',
+        description: 'Combo #2 descripción',
+        dices: [
+          { face: kick },
+          { face: kick },
+          { face: kick },
+          { face: punch },
+          { face: punch, damage: '4' },
+        ],
+      },
+      {
+        name: 'COMBO #3',
+        description: 'Combo #3 descripción',
+        dices: [
+          { face: punch },
+          { face: punch },
+          { face: punch },
+          { face: ironLever },
+          { face: ironLever, damage: '4' },
+        ],
+      },
+      {
+        name: 'COMBO #4',
+        description: 'Combo #4 descripción',
+        dices: [
+          { face: ironLever },
+          { face: ironLever },
+          { face: ironLever },
+          { face: punch },
+          { face: punch, damage: '5' },
+        ],
+      },
+      {
+        name: 'COMBO #5',
+        description: 'Combo #5 descripción',
+        dices: [
+          { face: kick },
+          { face: kick },
+          { face: kick },
+          { face: ironLever },
+          { face: ironLever, damage: '6' },
+        ],
+      },
+      {
+        name: 'COMBO #6',
+        description: 'Combo #6 descripción',
+        dices: [
+          { face: kick },
+          { face: kick },
+          { face: kick },
+          { face: ironLever },
+          { face: ironLever, damage: '6' },
+        ],
+      },
+      {
+        name: 'COMBO #7',
+        description: 'Combo #7 descripción',
+        dices: [
+          { face: ironLever },
+          { face: ironLever },
+          { face: ironLever },
+          { face: kick },
+          { face: kick, damage: '7' },
+        ],
+      },
+    ],
+    special: [
+      {
+        name: 'SPECIAL #1',
+        description: 'Special #1 descripción',
+        dices: [
+          { face: invisibility },
+          { face: invisibility },
+          { face: invisibility },
+          { face: punch },
+          { face: punch, damage: '1' },
+        ],
+      },
+      {
+        name: 'SPECIAL #2',
+        description: 'Special #2 descripción',
+        dices: [
+          { face: invisibility },
+          { face: invisibility },
+          { face: invisibility },
+          { face: kick },
+          { face: kick, damage: '1' },
+        ],
+      },
+      {
+        name: 'SPECIAL #3',
+        description: 'Special #3 descripción',
+        dices: [
+          { face: invisibility },
+          { face: invisibility },
+          { face: invisibility },
+          { face: ironLever },
+          { face: ironLever, damage: '1' },
+        ],
+      },
+    ],
+    fatalities: [
+      {
+        name: 'FATALITY #1',
+        description: 'FATALITY #1 descripción',
+        dices: [
+          { face: punch },
+          { face: punch },
+          { face: punch },
+          { face: punch },
+          { face: punch, damage: '5' },
+        ],
+      },
+      {
+        name: 'FATALITY #2',
+        description: 'FATALITY #2 descripción',
+        dices: [
+          { face: kick },
+          { face: kick },
+          { face: kick },
+          { face: kick },
+          { face: kick, damage: '7' },
+        ],
+      },
+      {
+        name: 'FATALITY #3',
+        description: 'FATALITY #3 descripción',
+        dices: [
+          { face: ironLever },
+          { face: ironLever },
+          { face: ironLever },
+          { face: ironLever },
+          { face: ironLever, damage: '7' },
+        ],
+      },
+    ],
   },
   /*attacks: [
     {
