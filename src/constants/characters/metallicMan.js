ba382876a092570250715fb06f11d8ecfbd8d6aa
Marc Romo <marc.romo@attackiq.com>
2022-07-25 18:41:40 +0200

    metallic man

--- src/constants/characters/metallicMan.js
+++ src/constants/characters/metallicMan.js
@@ -16,7 +16,85 @@ const metallicMan = {
   defenses: [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4],
   dices: [],
   faces: [punch, kick, eyeRay, defense, doubleDefense, errorSystem],
-  attacks: { basic: [] },
+  attacks: {
+    basic: [
+      {
+        name: 'BASIC #1',
+        description: 'Description Basic #1',
+        dices: [
+          { face: punch, damage: '1' },
+          { face: punch, damage: '2' },
+          { face: punch, damage: '3' },
+          { face: punch, damage: '4' },
+          { face: punch, damage: '5' },
+        ],
+      },
+      {
+        name: 'BASIC #2',
+        description: 'Description Basic #2',
+        dices: [
+          { face: kick, damage: '1' },
+          { face: kick, damage: '2' },
+          { face: kick, damage: '3' },
+          { face: kick, damage: '4' },
+          { face: kick, damage: '5' },
+        ],
+      },
+      {
+        name: 'BASIC #3',
+        description: 'Description Basic #3',
+        dices: [
+          { face: eyeRay },
+          { face: eyeRay, damage: '2' },
+          { face: eyeRay, damage: '4' },
+          { face: eyeRay, damage: '8' },
+          { face: eyeRay, damage: '16' },
+        ],
+      },
+    ],
+    combo: [
+      {
+        name: 'COMBO #1',
+        description: 'Description Combo #1',
+        dices: [{ face: punch }, { face: kick }, { face: eyeRay, damage: '3' }],
+      },
+      {
+        name: 'COMBO #2',
+        description: 'Description Combo #1',
+        dices: [
+          { face: punch },
+          { face: punch },
+          { face: kick },
+          { face: kick, damage: '4' },
+        ],
+      },
+      {
+        name: 'COMBO #3',
+        description: 'Description Combo #3',
+        dices: [
+          { face: punch },
+          { face: punch },
+          { face: kick },
+          { face: kick },
+          { face: eyeRay, damage: '5' },
+        ],
+      },
+    ],
+    special: [
+      {
+        name: 'ERROR SYSTEM',
+        description:
+          'Cortocircuito, se cancela tu ataque y no puedes defender en tu siguiente turno',
+        dices: [{ face: errorSystem }],
+      },
+      {
+        name: 'FATAL ERROR',
+        description:
+          'Te atacas a ti mismo y no puedes defender tu siguiente ataque',
+        dices: [{ face: errorSystem }, { face: errorSystem }],
+      },
+    ],
+  },
 }
 
 export default metallicMan
